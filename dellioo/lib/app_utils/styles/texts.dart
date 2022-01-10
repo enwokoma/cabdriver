aceb2664cc0da52d80af8724a04ff1bb198f5269
ebube <okochaebube@gmail.com>
2022-01-10 15:16:18 +0100

    added splash screen

---
+++ dellioo/lib/app_utils/styles/texts.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'colors.dart';
+
+TextStyle mainAppNameStyle = GoogleFonts.dmSans(
+  textStyle: const TextStyle(
+      color: dlColorWhite,
+      fontSize: 48,
+      fontWeight: FontWeight.w700
+  ),
+);
---
+++ dellioo/lib/ui/views/splash_screen/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'package:dellioo/app_utils/styles/colors.dart';
+import 'package:dellioo/app_utils/styles/texts.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        height: size.height,
+        width: size.width,
+        color: dlColorPrimary400,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Dellioo',
+                style: mainAppNameStyle,
+              ),
+            ),
+            const Positioned(
+              bottom: 50,
+              right: 0,
+              left: 0,
+              child: SpinKitThreeBounce(
+                color: dlColorWhite,
+                size: 24.0,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
