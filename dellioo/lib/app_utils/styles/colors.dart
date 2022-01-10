6cc3433a1dab656ccca4d620c18c086d73ad7fb8
ebube <okochaebube@gmail.com>
2022-01-10 08:52:48 +0100

    added colors

---
+++ dellioo/lib/app_utils/styles/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+//Primary Colors
+const dlColorPrimary100 = Color(0xFFCEC1EA);
+const dlColorPrimary200 = Color(0xFFAE98DD);
+const dlColorPrimary300 = Color(0xFF8E6ED0);
+const dlColorPrimary400 = Color(0xFF8E6ED0);
+const dlColorPrimary500 = Color(0xFF5A30BC);
+const dlColorPrimary600 = Color(0xFF512BB6);
+const dlColorPrimary700 = Color(0xFF4224AD);
+
+//Neutral Colors
+const dlColorWhite = Color(0xFFFFFFFF);
+const dlColorBlackGrey7 = Color(0xFFFBFBFB);
+const dlColorBlackGrey6 = Color(0xFFEDEDED);
+const dlColorBlackGrey5 = Color(0xFFDDDDDD);
+const dlColorBlackGrey4 = Color(0xFFBEBEBE);
+const dlColorBlackGrey3 = Color(0xFF818181);
+const dlColorBlackGrey2 = Color(0xFF424242);
+const dlColorBlackGrey1 = Color(0xFF222222);
+
+
